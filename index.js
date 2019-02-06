b3b5cb95229493ddcb10cb2712659e71f435f013
Hana Saori Keiningham <hanakein@gmail.com>
2019-02-06 21:25:00 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-parseInt(1 + 80) // 81
-paraseInt(60 - 40) // 20
-pasrseFloat(2 * 3.4) // 6.8 (there's that floating-point arithmetic again...)
-parseInt(5.0 / 2.5) // 2
+parseInt(1 += 80) // 81
+paraseInt(60 -= 40) // 20
+pasrseFloat(2 *= 3.4) // 6.8 (there's that floating-point arithmetic again...)
+parseInt(5.0 /= 2.5) // 2
